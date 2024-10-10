8488e62f3474ce196b9cc38b0d1a622cbc39d348
mianazeemdaula <mianazeemdaula@gmail.com>
2024-10-10 09:41:33 +0500

    nashta

--- routes/web.php
+++ routes/web.php
@@ -9,5 +9,18 @@ Route::middleware(['verify.shopify'])->group(function () {
     Route::get('/callback', 'App\Http\Controllers\ShopifyStoreController@callback')->name('callback');
     Route::get('/orders', 'App\Http\Controllers\ShopifyStoreController@orders')->name('orders');
     Route::get('/export-orders', 'App\Http\Controllers\ShopifyStoreController@exportorders')->name('exportorders');
+});
 
+Route::middleware(['auth.webhook'])->group(function () {
+    Route::post('/app/customer/data', function(Request $request){
+        return response()->json(['message' => 'data fetched successfully']);
+    });
+
+    Route::post('/app/customer/erasure', function(Request $request){
+        return response()->json(['message' => 'Data removed successfully']);
+    });
+
+    Route::post('/app/shop/erasure', function(Request $request){
+        return response()->json(['message' => 'Data removed successfully']);
+    });
 });
