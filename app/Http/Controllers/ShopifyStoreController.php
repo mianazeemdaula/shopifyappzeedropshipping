688719e4b27cc3cafc4b69cb83df5da598f8a7ea
mianazeemdaula <mianazeemdaula@gmail.com>
2024-10-21 13:57:22 +0500

    ids

--- app/Http/Controllers/ShopifyStoreController.php
+++ app/Http/Controllers/ShopifyStoreController.php
@@ -25,6 +25,9 @@ class ShopifyStoreController extends Controller
     public function exportorders(Request $request)
     {
         $shop = $request->user();
+        dd(
+            $request->ids
+        );
         $ids = is_array($request->ids) ?  implode(',',$request->ids) : [$request->ids];
         $data =  $shop->api()->rest('GET', '/admin/api/2024-07/orders.json', ['ids' => $ids,'status' => 'any']);
         dd([
