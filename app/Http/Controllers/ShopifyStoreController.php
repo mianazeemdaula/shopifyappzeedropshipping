e4198512e90eaf4c4eaeba4d0510ac8f3df926ca
mianazeemdaula <mianazeemdaula@gmail.com>
2024-10-21 13:53:53 +0500

    orders

--- app/Http/Controllers/ShopifyStoreController.php
+++ app/Http/Controllers/ShopifyStoreController.php
@@ -27,7 +27,7 @@ class ShopifyStoreController extends Controller
         $shop = $request->user();
         $ids = is_array($request->ids) ?  implode(',',$request->ids) : [$request->ids];
         $data =  $shop->api()->rest('GET', '/admin/api/2024-07/orders.json', ['ids' => $ids,'status' => 'any']);
-        // dd($data);
+        dd($data);
         if(isset($data['status']) &&  $data['status'] == 200){
             $orders =  $data['body']['orders'];
             $orders = json_encode($orders);
